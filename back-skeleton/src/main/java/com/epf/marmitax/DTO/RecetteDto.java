a8cde00526c808c4ad5143e9b9af6c4ae2879a1a
lsegouin <lou-anne.segouin@epfedu.fr>
2025-10-07 10:42:38 +0200

    feat: recettemapper fait pour evannnnnnnnnnnnnnnnn

--- back-skeleton/src/main/java/com/epf/marmitax/DTO/RecetteDto.java
+++ back-skeleton/src/main/java/com/epf/marmitax/DTO/RecetteDto.java
@@ -1,17 +1,21 @@
 package com.epf.marmitax.DTO;
 
 import com.epf.marmitax.models.Ustensile;
+
+import lombok.Builder;
+
 import com.epf.marmitax.models.Ingredient;
 
 import java.util.List;
 
+@Builder
 public record RecetteDto(
     Long id_recette_Dto,
     String nom_recette_Dto,
     String image_recette_Dto,
     Integer duree_recette_Dto,
     Integer prix_recette_Dto,
-    Integer difficulté_recette_Dto,
+    Integer difficulte_recette_Dto,
     String contenu_recette_Dto,
     List<Ustensile> ustensiles_Dto,
     List<Ingredient> ingredients_Dto
--- back-skeleton/src/main/java/com/epf/marmitax/DTO/RecetteMapper.java
+++ back-skeleton/src/main/java/com/epf/marmitax/DTO/RecetteMapper.java
@@ -1,5 +1,32 @@
 package com.epf.marmitax.DTO;
 
-public class RecetteMapper {
+import java.io.IOException;
+import com.epf.marmitax.models.Recette;
 
+public class RecetteMapper {
+    public static Recette fromDto(RecetteDto dto, Long id) throws IOException {
+        return Recette.builder()
+            .id_recette_model(id)
+            .nom_recette_model(dto.nom_recette_Dto())
+            .ingredients_model(dto.ingredients_Dto())
+            .ustensiles_model(dto.ustensiles_Dto())
+            .duree_recette_model(dto.duree_recette_Dto())
+            .difficulte_recette_model(dto.difficulte_recette_Dto())
+            .prix_recette_model(dto.prix_recette_Dto())
+            .image_recette_model(dto.image_recette_Dto())
+            .build();
+    }
+
+    public static RecetteDto toDto(Recette recette){
+        return RecetteDto.builder()
+            .id_recette_Dto(recette.getId_recette_model())
+            .nom_recette_Dto(recette.getNom_recette_model())
+            .ingredients_Dto(recette.getIngredients_model())
+            .ustensiles_Dto(recette.getUstensiles_model())
+            .duree_recette_Dto(recette.getDuree_recette_model())
+            .difficulte_recette_Dto(recette.getDifficulte_recette_model())
+            .prix_recette_Dto(recette.getPrix_recette_model())
+            .image_recette_Dto(recette.getImage_recette_model())
+            .build();
+    }
 }
